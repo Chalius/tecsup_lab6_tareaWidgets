3baddd4f86e64520018218066adb9ccb4effdaa2
gonzalo <gonzalo.quispe.f@tecsup.edu.pe>
2019-10-02 23:08:04 -0500

    coordenadas, ciudad y distrito en activity principal, se actualiza automaticamente

--- app/src/main/java/com/example/lab6_tareawidgets/MainActivity.java
+++ app/src/main/java/com/example/lab6_tareawidgets/MainActivity.java
@@ -14,9 +14,11 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -27,13 +29,13 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnGPS;
-    TextView tvUbicacion;
+    TextView tvUbicacion,txtciudad,txtdistrito;
+
 
     double latitud=0;
     double longitud;
+    String ubicacion[] ;
 
-    TextView txtciudad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,93 +43,54 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tvUbicacion = findViewById(R.id.tvUbicacion);
-        btnGPS = findViewById(R.id.button);
         txtciudad = findViewById(R.id.txtciudad);
+        txtdistrito = findViewById(R.id.txtdistrito);
 
 
+        // Acquire a reference to the system Location Manager
+        LocationManager locationManager = (LocationManager) MainActivity.this.getSystemService(Context.LOCATION_SERVICE);
 
+        // Define a listener that responds to location updates
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        btnGPS.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                // Acquire a reference to the system Location Manager
-                LocationManager locationManager = (LocationManager) MainActivity.this.getSystemService(Context.LOCATION_SERVICE);
-
-                // Define a listener that responds to location updates
-                LocationListener locationListener = new LocationListener() {
-                    public void onLocationChanged(Location location) {
-                        // Called when a new location is found by the network location provider.
-
-                        //makeUseOfNewLocation(location);
-                        latitud = location.getLatitude();
-                        longitud = location.getLongitude();
-                        tvUbicacion.setText(""+latitud+" "+longitud);
-
-                    }
-
-                    public void onStatusChanged(String provider, int status, Bundle extras) {}
-
-                    public void onProviderEnabled(String provider) {}
-
-                    public void onProviderDisabled(String provider) {}
-                };
-
-                // Register the listener with the Location Manager to receive location updates
-                int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this,
-                        Manifest.permission.ACCESS_FINE_LOCATION);
-                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-
+                //makeUseOfNewLocation(location);
+                latitud = location.getLatitude();
+                longitud = location.getLongitude();
+                tvUbicacion.setText(""+latitud+" "+longitud);
                 // averiguar ciudad
 
                 try {
                     if(latitud!=0) {
-                        String ciudad = getMyCity(latitud, longitud);
-                        txtciudad.setText(ciudad);
+                        ubicacion = getMyCity(latitud, longitud);
+                        txtciudad.setText(ubicacion[0]);
+                        txtdistrito.setText(ubicacion[1]);
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-
-
             }
-        });
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+
+        // Register the listener with the Location Manager to receive location updates
+        int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
 
 
 
+        /*
         int permissionCheck = ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION);
+        */
 
         if(permissionCheck== PackageManager.PERMISSION_DENIED){
             if(ActivityCompat.shouldShowRequestPermissionRationale(this,
@@ -144,17 +107,25 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public String getMyCity(Double latitud,Double longitud) throws IOException {
+    public String[] getMyCity(Double latitud,Double longitud) throws IOException {
         String myCity = "";
+        String distrito = "";
+        String completo = "";
+        String ubicacion[] = new String[2];
+
 
         Geocoder geocoder = new Geocoder(
                 MainActivity.this,
                 Locale.getDefault());
         List<Address> addresses = geocoder.getFromLocation(latitud,longitud,1);
-        //String address = addresses.get(0).getAddressLine(0);
-        myCity = addresses.get(0).getLocality();//getlocality se puede cambiar por otra cosa como:pais
+        myCity = addresses.get(0).getAdminArea();//getlocality se puede cambiar por otra cosa como:pais
+        distrito = addresses.get(0).getSubLocality();
+        completo =addresses.toString(); //toda la información extraida de las coordenadas
 
-        return myCity;
+        ubicacion[0] = myCity;
+        ubicacion[1] = distrito;
+
+        return ubicacion;
     }
 
 }
